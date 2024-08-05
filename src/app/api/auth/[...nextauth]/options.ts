785587a37980a9ea451b04784263eaa6091d86e7
Vaibhav Gupta <109146556+vaibhavgupta5@users.noreply.github.com>
2024-08-06 01:34:21 +0530

    Update options.ts

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -54,7 +54,7 @@ export const authOptions: NextAuthOptions = {
   //     clientId: process.env.GOOGLE_CLIENT_ID as string,
   //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
   //   }),
-  // ],
+  ],
 
   callbacks: {
     // async signIn({ user, account, profile }) {
